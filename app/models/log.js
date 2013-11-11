7b3b83dfae8ae9305feeb8aea8ee01f5fd7c475c
Daniel Lee Hwang <danielleehwang@gmail.com>
2013-11-10 22:42:06 -0800

    fix whitespace

--- app/models/log.js
+++ app/models/log.js
@@ -32,80 +32,80 @@ LogSchema.virtual('timestamp_unix').get(function() {
 LogSchema.statics = {
     getLastSeen: function(channel, nick, from, callback) {
 	this.where('nick').equals(nick)
-        .where('_type').in(['PartLog', 'QuitLog', 'KickLog', 'KillLog'])
-	    .where('channel').in(['all', channel])
-	    .where('timestamp').lte(from)
+            .where('_type').in(['PartLog', 'QuitLog', 'KickLog', 'KillLog'])
+            .where('channel').in(['all', channel])
+            .where('timestamp').lte(from)
 	    .sort({'timestamp': 'descending'})
-        .select('timestamp')
+            .select('timestamp')
 	    .findOne(function(err, lastSeen) {
-			if (err) return;
-			if (!lastSeen) return;
-			callback(lastSeen.timestamp);
+                    if (err) return;
+                    if (!lastSeen) return;
+                    callback(lastSeen.timestamp);
 	    });
     },
     getLogsFrom: function(channel, from, to, strict, callback) {
-		if (strict) {
-		    to.seconds(to.seconds()+1);
-		}
+        if (strict) {
+            to.seconds(to.seconds()+1);
+        }
 
-		this.find()
-		    .where('channel').in(['all', channel])
-		    .where('timestamp').lte(to).gte(from)
-		    .sort({'timestamp': 'ascending'})
-		    .exec(function(err, logs) {
-				if (err) return;
-				if (!logs) return;
+        this.find()
+            .where('channel').in(['all', channel])
+            .where('timestamp').lte(to).gte(from)
+	    .sort({'timestamp': 'ascending'})
+	    .exec(function(err, logs) {
+                    if (err) return;
+                    if (!logs) return;
 
-				if (strict) {
-				    var unixOffset;
+                    if (strict) {
+                        var unixOffset;
 
-				    unixOffset = moment(logs[0].timestamp);
-				    var fromUnixOffset = from.valueOf();
-				    while (unixOffset != fromUnixOffset) {
-						logs.shift();
-						if (!logs.length) break;
-							unixOffset = moment(logs[0].timestamp).valueOf();
-				    }
+                        unixOffset = moment(logs[0].timestamp);
+                        var fromUnixOffset = from.valueOf();
+                        while (unixOffset != fromUnixOffset) {
+                            logs.shift();
+                            if (!logs.length) break;
+                            unixOffset = moment(logs[0].timestamp).valueOf();
+                        }
 
-				    unixOffset = moment(logs[logs.length-1].timestamp);
-				    to.seconds(to.seconds()-1);
-				    var toUnixOffset = to.valueOf();
-				    while (unixOffset != toUnixOffset) {
-						logs.pop();
-						if (!logs.length) break;
-						unixOffset = moment(logs[logs.length-1].timestamp).valueOf();
-				    }
-				}
-			
-				callback(logs);
-		    });
+                        unixOffset = moment(logs[logs.length-1].timestamp);
+                        to.seconds(to.seconds()-1);
+                        var toUnixOffset = to.valueOf();
+                        while (unixOffset != toUnixOffset) {
+                            logs.pop();
+                            if (!logs.length) break;
+                            unixOffset = moment(logs[logs.length-1].timestamp).valueOf();
+                        }
+                    }
+                    
+                    callback(logs);
+                });
     },
     getUniqueTimestamps: function(channel, from, to, callback) {
-		var deferred = Q.defer();
-		this.find()
-		    .where('channel').in(['all', channel])
-		    .where('timestamp').lte(to).gte(from)
-		    .sort({'timestamp': 'ascending'})
-	        .select('timestamp')
-		    .exec(function(err, logs) {
-				if (err) return;
-				if (!logs) return;
+        var deferred = Q.defer();
+        this.find()
+	    .where('channel').in(['all', channel])
+	    .where('timestamp').lte(to).gte(from)
+	    .sort({'timestamp': 'ascending'})
+	    .select('timestamp')
+	    .exec(function(err, logs) {
+                    if (err) return;
+                    if (!logs) return;
 
-				var uniques = _.uniq(logs, function(log) {
-				    return moment(log.timestamp).format('YYYY-MM-DD');
-				});
-				uniques = _.map(uniques, function(log) {
-				    return moment(log.timestamp);
-				});
-				async.each(uniques, callback, function(aerr) {
-				    if (aerr) {
-						deferred.reject(aerr);
-				    } else {
-						deferred.resolve(true);
-				    }
-				});
-		    });
-		return deferred.promise;
+                    var uniques = _.uniq(logs, function(log) {
+                            return moment(log.timestamp).format('YYYY-MM-DD');
+                        });
+                    uniques = _.map(uniques, function(log) {
+                            return moment(log.timestamp);
+                        });
+                    async.each(uniques, callback, function(aerr) {
+                            if (aerr) {
+                                deferred.reject(aerr);
+                            } else {
+                                deferred.resolve(true);
+                            }
+                        });
+                });
+        return deferred.promise;
     },
     existsLogsOn: function(channel, on, callback) {
 	var from = moment(on).startOf('day').toDate();
@@ -113,12 +113,12 @@ LogSchema.statics = {
 	this.find()
 	    .where('channel').in(['all', channel])
 	    .where('timestamp').lte(to).gte(from)
-        .select('timestamp')
+            .select('timestamp')
 	    .count(function(err, count) {
-			if (count >= 1) {
-			    callback();
-			}
-	    });
+                    if (count >= 1) {
+                        callback();
+                    }
+                });
     }
 };
 
